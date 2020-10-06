4c507c1b4adb7ffab77bf2c80731a0cfcb391204
ot87 <oleksandr.tsyrkul@gmail.com>
2020-10-06 10:05:43 +0300

    Fix demo data

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import API from './api';
 // TODO demo profile
 API.init('Profile 1');
 // TODO with demo schema
-if (!API.loadData().length) {
+if (!API.loadData().schemas.length) {
   API.saveData({"schemas":[{"name":"Demo Schema 1","description":"Demo Schema 1 Description","items":[{"id":1,"name":"Item 111","quantity":"Quantity 1","time":"Time 1"},{"id":6,"name":"111","quantity":"111","time":"111"},{"id":7,"name":"222","quantity":"222","time":"222"}],"id":1}]});
 }
 
