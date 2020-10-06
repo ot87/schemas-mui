6834c472847d2e945c2482859f3ab17b930cd071
ot87 <oleksandr.tsyrkul@gmail.com>
2020-10-06 10:57:38 +0300

    Updates

---
+++ precache-manifest.b9cebca76c3abdd6cf6248ed65575a30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe34aa31d633663f420a27fbf3710a9d",
+    "url": "/schemas/index.html"
+  },
+  {
+    "revision": "69205608d74a2796b84a",
+    "url": "/schemas/static/css/main.619c2c04.chunk.css"
+  },
+  {
+    "revision": "b3797a0de71b69a77d62",
+    "url": "/schemas/static/js/2.21eb91bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/schemas/static/js/2.21eb91bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69205608d74a2796b84a",
+    "url": "/schemas/static/js/main.fa836f6f.chunk.js"
+  },
+  {
+    "revision": "5addc3ca4d98c48c03ba",
+    "url": "/schemas/static/js/runtime-main.7679cbe4.js"
+  }
+]);
